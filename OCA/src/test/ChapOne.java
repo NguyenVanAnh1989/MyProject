5589ecba6a74a2793e7b03f0e7d52afae3dd5b73
tungle-ntq <tung.le1@ntq-solution.com.vn>
2018-08-13 10:36:38 +0700

    save code

--- OCA/src/test/ChapOne.java
+++ OCA/src/test/ChapOne.java
@@ -1,6 +1,7 @@
 package test;
 
 public class ChapOne {
+	String referencesType;
 	boolean boolType;
 	byte byteType;
 	short shortType;
@@ -9,7 +10,7 @@ public class ChapOne {
 	float floatValue;
 	double doubleType;
 	char charType;
-
+	int checkUnderScore;
 	public void primitivesDeclare() {
 		// java have 8 primitives type
 		boolType = true; // false
@@ -20,6 +21,26 @@ public class ChapOne {
 		floatValue = 32000.234f; // floating point need declare f,F after number declare
 		doubleType = 10000.1234; // double value
 		charType = 'a'; // character type
+		checkUnderScore= 2_3_4_5_6; // You can put underscore any where except begin,end, left-right decimaPoint
+	}
+	
+	public void referenceTypes() {
+		Object firstObject = new Object();
+		Object asignToOtherObject = firstObject; //first way value assigned to reference other
+		Object usingNewKeyWord = new Object(); // second way value assign by new Object
 	}
 
+	public void differencesBeetwenPrimitivesReference() {
+		referencesType = null; // reference can assign to null
+		// intType = null;  cannot assign primitives to null
+		referencesType.length(); //
+		// intType.length(); primitive don't have call method
+		// remember that all primitive start with lowercase
+	}
+	
+	public void declareAndInitializingVariable() {
+		
+		
+	}
+	
 }

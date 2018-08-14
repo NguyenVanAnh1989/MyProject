a12b07c82d30903caded3432fbfd6d1cd12e01b5
tungle-ntq <tung.le1@ntq-solution.com.vn>
2018-08-14 11:20:31 +0700

    fix all

--- OCA/src/main/Main.java
+++ OCA/src/main/Main.java
@@ -1,25 +1,18 @@
 package main;
 
 import test.ChapOne;
+import test.ChapTwo;
 
 public class Main {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ChapOne chapOne = new ChapOne();
-		chapOne = null;
-//		System.gc();
-		ChapOne chapTwo = new ChapOne();
-		chapTwo = null;
-//		System.gc();
-		ChapOne chapThree = new ChapOne();
-		chapThree = null;
-//		System.gc();
-		ChapOne chapFour = new ChapOne();
-		chapFour = null;
-		System.gc();
-//		chapone.declareAndInitializingVariable();
-		System.out.println("" + "data will call");
+//		ChapOne chapOne = new ChapOne();
+//		chapOne = null;
+//		chapOne.declareAndInitializingVariable();
+		ChapTwo chapTwo = new ChapTwo(ChapTwo.DayTest.DAYFIVE);
+		chapTwo.declareSomeOperator();
+//		System.out.println("" + "data will call");
 
 	}
 
--- OCA/src/test/ChapTwo.java
+++ OCA/src/test/ChapTwo.java
@@ -1,5 +1,56 @@
 package test;
 
 public class ChapTwo {
+	
+	DayTest enumTest;
+	
+	// Some operator
+	public ChapTwo(DayTest enumTest) {
+		this.enumTest = enumTest;
+	}
+	
+	
+	public void declareSomeOperator() {
+		int addition = 0 + 0;
+		int subtraction = 0 - 0;
+		int multiplication = 0 * 0;
+		int division = 0 / 1;
+		int modulus = 0 % 1;
+		int unary = 0;
+		int orderOperator = (10 + 10 - 10) * 10 / 10 % --unary + unary++;
+		// System.out.println(orderOperator);
+		// System.out.println(10 % );
+		long logType = 3000000000l;
+		float ftype = 232131244;
+		// System.out.printf("%f",logType + ftype); // this will auto convert to float
+		// -> convert double
+		int positive = +10;
+		int negative = -10;
+		int cast = (int) 100L;
+		long m = 100;
+		int n = 100;
+		n += 100; // use this way for convert numeric operator
+		boolean logicBitWishAnd = true & false; // & => (true & true => true)(true & false => false)(false & true =>
+												// false)(false & false => false) // same &&
+		boolean logicBitWishOr = true | false; // & => (true & true => true)(true & false => false)(false & true =>
+												// false)(false & false => false) // same ||
+		boolean logicBitWishXor = true ^ false; // & => (true & true => false)(true & false => true)(false & true =>
+												// true)(false & false => true
+	}
+
+	public void statementCheck(boolean check) {
+		if (check) {
+
+		} else if (check) {
+			
+		} else {
+			
+		}
+	}
+	
+	public static enum DayTest {
+		DAYONE, DAYTWO, DAYTHREE, DAYFOUR, DAYFIVE, DAYSIX, DAYSEVEN
+	}
 
 }
+
